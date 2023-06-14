a396b24605474e90e7e062172fbb85fa798cab50
broline <brolineoronu3@gmail.com>
2023-06-14 19:07:00 +0300

    replace

--- RecipeRealm/core/static/core/scripts/recipe.js
+++ RecipeRealm/core/static/core/scripts/recipe.js
@@ -64,5 +64,18 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
-
-
+  
+//dark theme
+  const body = document.querySelector("body");
+  const toggle = document.querySelector("#toggle");
+  const sunIcon = document.querySelector(".toggle .bxs-sun");
+  const moonIcon = document.querySelector(".toggle .bx-moon");
+  
+  toggle.addEventListener("change", () => {
+  
+      body.classList.toggle("dark");
+      sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+      moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+  
+  
+  });
